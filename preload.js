d210f3814c2a21acf6e7b5cb984c61d377b4edd7
Emerson Shatouhy <emersont2003@gmail.com>
2022-01-16 20:39:52 -0500

    commiy

--- preload.js
+++ preload.js
@@ -1,7 +1,36 @@
 const { F1TelemetryClient } = require('f1-2021-udp');
-const client= new F1TelemetryClient();
+const client= new F1TelemetryClient({port:20770});
+var curDriver = 0;
 client.on('carTelemetry',function(data) {
-    document.getElementById("acclerator").style.width = data.m_carTelemetryData[0].m_throttle * 100 + "%";
-    document.getElementById("brake").style.width = data.m_carTelemetryData[0].m_brake * 100 + "%";
+    document.getElementById("acclerator").style.width = data.m_carTelemetryData[curDriver].m_throttle * 100 + "%";
+    document.getElementById("brake").style.width = data.m_carTelemetryData[curDriver].m_brake * 100 + "%";
+});
+var run = false;
+client.on('participants', function(data) {
+    if(run) { return;
+    } else {
+     for(i=0;i<data.m_participants.length;i++) {
+        var dList = document.getElementById("dSelect");
+        var option = document.createElement("option");
+        option.text = data.m_participants[i].m_name;
+        option.id = i;
+        dList.add(option);
+     }
+     run = true;
+    }
+});
+
+client.on('lapData',function(data) {
+    document.getElementById("pos").innerHTML = data.m_lapData[curDriver].m_carPosition;
 })
+
+
+document.addEventListener('input', function (event) {
+    var dList = document.getElementById("dSelect");
+    var id = dList.options[dList.selectedIndex].id;
+	if (event.target.id !== 'dSelect') return;
+    curDriver= id;
+    console.log(id);
+}, false);
+
 client.start();
