4ff93924efc8b237aaeabbf9591677da6f674663
AndrewR15 <Andrew.roush@austin.rr.com>
2022-01-20 00:15:22 -0500

    lots of stuff and broken deltas

--- DeltaCleanupWorker.js
+++ DeltaCleanupWorker.js
@@ -1,4 +1,4 @@
-const OLD_DISTANCES_KEPT = 5; // Number of distances that should be kept in common between all cars
+const { COMMON_DATAPOINTS } = require('./config.json');
 
 /**
  * Handles a message from the parent
@@ -33,8 +33,8 @@ function cleanupSet(map) {
         }
     })
 
-    if (distancesAllHave.size > OLD_DISTANCES_KEPT) {
-        let numToRemove = distancesAllHave.size - OLD_DISTANCES_KEPT;
+    if (distancesAllHave.size > COMMON_DATAPOINTS) {
+        let numToRemove = distancesAllHave.size - COMMON_DATAPOINTS;
         for (let i = 0; i < numToRemove; i++) {
             let minimumDistance = null;
             distancesAllHave.forEach((distance) => {
@@ -57,3 +57,71 @@ function cleanupSet(map) {
 
     return map;
 }
+
+/*
+// Test 1, not enough to delete so set should be the same even though they all contain the same values
+let testMap1 = new Map();
+let copyMap1 = new Map();
+testMap1.set(1, new Map());
+copyMap1.set(1, new Map());
+testMap1.set(2, new Map());
+copyMap1.set(2, new Map());
+testMap1.set(3, new Map());
+copyMap1.set(3, new Map());
+testMap1.get(1).set(100, 550);
+copyMap1.get(1).set(100, 550);
+testMap1.get(2).set(100, 600);
+copyMap1.get(2).set(100, 600);
+testMap1.get(3).set(100, 700);
+copyMap1.get(3).set(100, 700);
+testMap1 = cleanupSet(testMap1);
+// Hand inspect output using break points
+
+// Test 2, should delete one
+let testMap2 = new Map();
+testMap2.set(5, new Map());
+testMap2.set(6, new Map());
+testMap2.set(7, new Map());
+
+for (let i = 0; i <= COMMON_DATAPOINTS; i++) {
+    testMap2.get(5).set(i, i + 100);
+    testMap2.get(6).set(i, i + 102);
+    testMap2.get(7).set(i, i + 104);
+}
+
+let copyMap2 = new Map();
+copyMap2.set(5, new Map());
+copyMap2.get(5).set(0, 100);
+copyMap2.set(6, new Map());
+copyMap2.get(6).set(0, 102);
+copyMap2.set(7, new Map());
+copyMap2.get(7).set(0, 104);
+testMap2 = cleanupSet(testMap2);
+// Hand inspect output using break points
+
+// Test 3, should delete the COMMON_DATAPOINTS constant number
+let testMap3 = new Map();
+testMap3.set(10, new Map());
+testMap3.set(11, new Map());
+testMap3.set(12, new Map());
+
+for (let i = 0; i < 2 * COMMON_DATAPOINTS; i++) {
+    testMap3.get(10).set(i + 1, i + 200);
+    testMap3.get(11).set(i + 1, i + 202);
+    testMap3.get(12).set(i+ 1, i + 204);
+}
+
+let copyMap3 = new Map();
+copyMap3.set(10, new Map());
+copyMap3.set(11, new Map());
+copyMap3.set(12, new Map());
+
+for (let i = COMMON_DATAPOINTS; i < 2 * COMMON_DATAPOINTS; i++) {
+    copyMap3.get(10).set(i + 1, i + 200);
+    copyMap3.get(11).set( i + 1, i + 202);
+    copyMap3.get(12).set(i + 1, i + 204);
+}
+
+testMap3 = cleanupSet(testMap3);
+// Hand inspect output using break points
+*/
