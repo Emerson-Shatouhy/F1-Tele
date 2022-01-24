7fd3d9c1e033d25319edeb8fc15005511c906573
AndrewR15 <Andrew.roush@austin.rr.com>
2022-01-23 19:24:54 -0500

    td

---
+++ src/js/renderer.js
@@ -0,0 +1,5 @@
+const {deltaBetween, distanceReceived} = require('./DeltaCalculation/DeltaCalculator.js');
+const {} =require('../../config.json');
+//window.electron.doThing();
+
+document.getElementById("test").innerHTML = window.electron.doThing();
